b4d7bd83f6e7ec742911c1cee21fc91466cd2b58
ahsan <ahsanzafar96@icloud.com>
2024-09-18 19:32:48 +0100

    Model updates

--- main/class_upvote/models.py
+++ main/class_upvote/models.py
@@ -5,24 +5,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name  # Display category name in Django admin
+    
+class Attachment(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    attachment_name = models.CharField(max_length=30)
+    attachment_type = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.attachment_name} ({self.attachment_type})"
 
 
 class Weapon(models.Model):
     weapon_name = models.CharField(max_length=30)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    attachment = models.ForeignKey(Attachment, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.weapon_name  # Display weapon name in Django admin
-
-
-class Attachment(models.Model):
-    weapon = models.ForeignKey(Weapon, on_delete=models.CASCADE)
-    attachment_name = models.CharField(max_length=30)
-    attachment_type = models.CharField(max_length=30)
-    description = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.attachment_name} ({self.attachment_type})"
 
 
 class AttachmentType(models.Model):
